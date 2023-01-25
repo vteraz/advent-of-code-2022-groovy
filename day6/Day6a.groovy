677fcb6194bbef23d6c3e0be19960d0fccdba887
Vasyl Teraz <vteraz@medallies.com>
2023-01-25 13:44:52 +0200

    Day 6

---
+++ day6/Day6a.groovy
@@ -0,0 +1,17 @@
+package day6
+
+def input = new File("./input.txt").text
+def windowSize = 4
+def window = new HashSet<String>()
+
+for (i in 0..<input.size() - windowSize + 1) {
+    input.substring(i, Math.min(input.size(), windowSize + i)).each { window.add(it) }
+    if (window.size() == windowSize) {
+        assert 1912 == i + windowSize
+        break
+    } else {
+        window = new HashSet<String>()
+    }
+}
+
+
